8041ebb2ab610515a5a46b75eaa6e34de0463ffe
vimalrijalmaqro <vimal.rijal@maqro.com.au>
2023-09-01 19:45:49 +1000

    first

---
+++ home.php
@@ -0,0 +1,83 @@
+
+
+  <section id="banner" class="vimal">
+   
+  <!-- Slider -->
+        <div id="main-slider" class="flexslider">
+            <ul class="slides">
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/1.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3>Find Job Easily</h3> 
+          <p>We create the opportunities</p> 
+           
+                </div>
+              </li>
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/2.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3>Specialize</h3> 
+          <p>Success depends on work</p>  
+           
+                </div>
+              </li>
+            </ul>
+        </div>
+  <!-- end slider -->
+ 
+  </section> 
+  <section id="call-to-action-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-sm-9">
+          <h3>Partner with Business Leaders</h3>
+          <p>Development of successful, long term, strategic relationships between customers and suppliers, based on achieving best practice and sustainable competitive advantage. In the business partner model, HR professionals work closely with business leaders and line managers to achieve shared organisational objectives.</p>
+        </div>
+       <!--  <div class="col-md-2 col-sm-3">
+          <a href="#" class="btn btn-primary">Read More</a>
+        </div> -->
+      </div>
+    </div>
+  </section>
+  
+  <section class="section-padding gray-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-title text-center">
+            <h2 >Popular Jobs</h2>  
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 ">
+          <?php
+          $sql = 'SELECT * FROM `tblcategory`';
+          $mydb->setQuery($sql);
+          $cur = $mydb->loadResultList();
+
+          foreach ($cur as $result) {
+              echo '<div class="col-md-3" style="font-size:15px;padding:5px">* <a href="' .
+                  web_root .
+                  'index.php?q=category&search=' .
+                  $result->CATEGORY .
+                  '">' .
+                  $result->CATEGORY .
+                  '</a></div>';
+          }
+          ?>
+        </div>
+      </div>
+ 
+    </div>
+  </section>    
+  <section id="content-3-10" class="content-block data-section nopad content-3-10">
+  <div class="image-container col-sm-6 col-xs-12 pull-left">
+    <div class="background-image-holder">
+
+    </div>
+  </div>
+
+</section>
+
+        
